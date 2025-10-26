136f772fb18761675ab242dea332a8b24fad0fb8
Ad.e <adedoc8@gmail.com>
2025-10-26 01:09:37 -0500

    refactor: move word count function to stats.py and import in main

--- main.py
+++ main.py
@@ -1,17 +1,14 @@
+from stats import get_num_words
+
 def get_book_text(filepath):
     """Takes a filepath as input and returns the contents of the file as a string."""
     with open(filepath, 'r') as file:
         return file.read()
 
-def count_words(text):
-    """Accepts text as a string and returns the number of words in the string."""
-    words = text.split()
-    return len(words)
-
 def main():
     """Counts and displays the number of words in the book."""
     book_text = get_book_text('books/frankenstein.txt')
-    num_words = count_words(book_text)
+    num_words = get_num_words(book_text)
     print(f"Found {num_words} total words")
 
 if __name__ == '__main__':
---
+++ stats.py
@@ -0,0 +1,4 @@
+def get_num_words(text):
+    """Accepts text as a string and returns the number of words in the string."""
+    words = text.split()
+    return len(words)
