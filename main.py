0e0bc7bc267db9d4039f336c352ff40fd65f11ea
Ad.e <adedoc8@gmail.com>
2025-10-26 01:12:31 -0500

    feat: add character count function and display character dictionary

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import get_num_words
+from stats import get_num_words, get_character_counts
 
 def get_book_text(filepath):
     """Takes a filepath as input and returns the contents of the file as a string."""
@@ -6,10 +6,13 @@ def get_book_text(filepath):
         return file.read()
 
 def main():
-    """Counts and displays the number of words in the book."""
+    """Counts words and characters in the book."""
     book_text = get_book_text('books/frankenstein.txt')
     num_words = get_num_words(book_text)
+    character_counts = get_character_counts(book_text)
+    
     print(f"Found {num_words} total words")
+    print(character_counts)
 
 if __name__ == '__main__':
     main()
--- stats.py
+++ stats.py
@@ -2,3 +2,14 @@ def get_num_words(text):
     """Accepts text as a string and returns the number of words in the string."""
     words = text.split()
     return len(words)
+
+def get_character_counts(text):
+    """Returns the number of times each character appears in the string."""
+    text = text.lower()
+    char_counts = {}
+    for char in text:
+        if char in char_counts:
+            char_counts[char] += 1
+        else:
+            char_counts[char] = 1
+    return char_counts
