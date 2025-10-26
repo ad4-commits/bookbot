b0fb9fec1a9628351d399de9a4bbb938cfea1d3d
Ad.e <adedoc8@gmail.com>
2025-10-26 00:50:41 -0500

    feat: add get_book_text function and main method

---
+++ main.py
@@ -0,0 +1,13 @@
+def get_book_text(filepath):
+    """Takes a filepath as input and returns the contents of the file as a string."""
+    with open(filepath, 'r') as file:
+        return file.read()
+
+def main():
+    """Uses get_book_text to read and print the entire contents of frankenstein.txt"""
+    book_text = get_book_text('frankenstein.txt')
+    print(book_text)
+
+# Call main at the bottom
+if __name__ == '__main__':
+    main()
